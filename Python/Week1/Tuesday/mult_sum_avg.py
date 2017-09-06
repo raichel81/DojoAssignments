b1b82224f92b115a6422ca87c5898656cbd3d704
Rachel <rai.chel.ann@gmail.com>
2017-09-05 19:37:50 -0700

    a few python assignments

---
+++ Python/Week1/Tuesday/mult_sum_avg.py
@@ -0,0 +1,33 @@
+'''Assignment: Multiples, Sum, Average
+This assignment has several parts. All of your code should be in one file that is well 
+commented to indicate what each block of code is doing and which problem you are solving. 
+Don't forget to test your code as you go!
+
+Multiples
+Part I - Write code that prints all the odd numbers from 1 to 1000. Use the for loop and 
+don't use a list to do this exercise.'''
+
+for i in range(1, 1001, 2):
+    print i
+
+'''Part II - Create another program that prints all the multiples of 5 
+from 5 to 1,000,000.
+'''
+for i in range(5,1000001,5):
+     print i
+
+'''Sum List
+Create a program that prints the sum of all the values in the list: '''
+
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)
+
+'''Average List
+Create a program that prints the average of the values in the list: '''
+
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)/len(a)
+
+
+
+
